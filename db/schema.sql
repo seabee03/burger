a4be7a3a0632b18a494ca6a77731cec9bc5cb0a7
DJ Prado <darcus.prado@gmail.com>
2019-08-19 12:53:12 -0500

    changed eaten to devoured in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE BURGER_DB;
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    eaten BOOLEAN DEFAULT false,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
